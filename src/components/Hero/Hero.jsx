2f8529b42ddd93908b381bb72b98b87e1648c8dd
munaghising001 <ghisingmuna001@gmail.com>
2026-04-20 13:43:00 +0545

    coding

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import Grocery from '../../assets/grocery.png'
-
+import Button from '../Button/Button'
 const Hero = () => {
     return (
         <section>
 
-            <div className='max-w-[1400px] mx-auto px-10'>
+            <div className='max-w-[1400px] mx-auto px-10 flex items-center'>
                 {/* hero Content */}
-                <div>
-
+                <div className='flex-1'>
+                    <span className='bg-orange-100 text-orange-500 text-lg px-5 py-2 rounded-full'> Export Best Quality...... </span>
+                    <h1 className='text-7xl font-bold'>
+                        Tasty Organic<span className='text-orange-500'> Fruits</span>  & <span className='text-orange-500'>Veggies</span> <br/>In Your City</h1>
+                    <p className='text-zinc-600 text-lg max-w-[530px] mt-5 mb-10'>
+                        Bred for a high content of beneficial substances. Our products are all fresh and healthy.</p>
+                    <Button/>
                 </div>
                 {/* Hero Image */}
-                <div>
-<img src= {Grocery} alt="Hero Image" />
+                <div className='flex-1'>
+                    <img src={Grocery} alt="Hero Image" />
                 </div>
             </div>
 
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,8 +5,8 @@ import Hero from '../Hero/Hero'
 const Home = () => {
   return (
     <div>
-      <Navbar/>
-      <Hero/>
+      <Navbar />
+      <Hero />
 
     </div>
   )
