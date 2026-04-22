972e374475cc88568680732eba6570d4cf58ec25
munaghising001 <ghisingmuna001@gmail.com>
2026-04-22 16:01:34 +0545

    responsive design

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,13 +5,13 @@ const Hero = () => {
     return (
         <section>
 
-            <div className='max-w-[1400px] mx-auto px-10 flex items-center'>
+            <div className='min-h screen max-w-[1400px] mx-auto px-10 flex md:flex-row flex-col items-center md:pt-25 pt-35'>
                 {/* hero Content */}
                 <div className='flex-1'>
                     <span className='bg-orange-100 text-orange-500 text-lg px-5 py-2 rounded-full'> Export Best Quality...... </span>
-                    <h1 className='text-7xl font-bold'>
+                    <h1 className='md:text-7xl/20 text-5xl/14 font-bold mt-5'>
                         Tasty Organic<span className='text-orange-500'> Fruits</span>  & <span className='text-orange-500'>Veggies</span> <br/>In Your City</h1>
-                    <p className='text-zinc-600 text-lg max-w-[530px] mt-5 mb-10'>
+                    <p className='text-zinc-600 md:text-lg text-md max-w-[530px] mt-5 mb-10'>
                         Bred for a high content of beneficial substances. Our products are all fresh and healthy.</p>
                     <Button/>
                 </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaHeart } from "react-icons/fa6";
 import { RiShoppingBag4Fill } from "react-icons/ri"
 import { FaSearch } from "react-icons/fa";
+import { TbMenu2 } from "react-icons/tb";
 
 
 const Navbar = () => {
+
+  const [showMenu, setShow] = useState(false);
+  const toggleMenu = () => {
+
+    setShowMenu(!showMenu);
+  }
   return (
-    <header className='bg-white'>
-      <nav className='max-w-[1100px] mx-auto px-10 h-[14vh] flex justify-between items-center'>
+    <header className='bg-white-300 fixed top-0 right-0 left-0'>
+      <nav className='max-w-[1100px] mx-auto px-10 md:h-[14vh] h-[12vh] flex justify-between items-center'>
         {/* logo */}
         <a href='#' className='text-3xl font-semibold'>
           Gr<span className='text-orange-500 uppercase'>o</span>cify
         </a>
 
         {/*Desktop Menu */}
-        <ul className='flex items-center gap-x-15'>
+        <ul className='md:flex items-center gap-x-15 hidden'>
           <li>
             <a href="#" className='font-semibold tracking-wider text-orange-800'>Home</a>
           </li>
@@ -32,9 +39,11 @@ const Navbar = () => {
         {/*Nav Action*/}
         <div className='flex items-center gap-x-5'>
         {/* Input Field */}
-        <div className='flex p-1 border-2 border-orange-500 rounded-full'>
+        <div className='md:flex p-1 border-2 border-orange-500 rounded-full hidden'>
           <input type='text' name='text' id='text' placeholder='Search.....' autoComplete='off' className='flex-1 h-[5vh] px-3 focus:outline-none'/>
-          <button className='bg-gradient-to-b from-red-600 to-orange-400 text-white w-10 h-10 flex justify-center items-center rounded-full text-xl'><FaSearch /></button>
+          <button className='bg-gradient-to-b from-red-600 to-orange-400 text-white w-10 h-10 flex justify-center items-center rounded-full text-xl'>
+            <FaSearch />
+          </button>
         </div>
           <a href='#' className='text-zinc-800 text-2xl'>
             <FaHeart />
@@ -42,7 +51,36 @@ const Navbar = () => {
             <a href='#' className='text-zinc-800 text-2xl'>
             <RiShoppingBag4Fill />
             </a>
+
+        {/* Hamburger */}
+
+            <a href='#' className='text-zinc-800 text-3xl md:hidden' onClick={toggleMenu}>
+              <TbMenu2 />
+            </a>
         </div>
+
+
+        {/* Mobile Menu */}
+         <ul className= {`flex flex-col  gap-y-12 bg-orange-500/15 backdrop-blur -xl rounded-lg-xl p-10 items-center gap-x-15  md:hidden absolute top-30 -left-full transform -translate-x-1/2 ${showMenu ? 'left-1/2' : ""}`}>       
+          <li>
+            <a href="#" className='font-semibold tracking-wider text-orange-800'>Home</a>
+          </li>
+          <li>
+            <a href="#" className='font-semibold tracking-wider text-zinc-800 hover:text-orange-500'>About US</a>
+          </li>
+          <li>
+            <a href="#" className='font-semibold tracking-wider text-zinc-800 hover:text-orange-500'>Process</a>
+          </li>
+          <li>
+            <a href="#" className='font-semibold tracking-wider text-zinc-800 hover:text-orange-500'>Contact Us</a>
+          </li>
+          <li className='flex p-1 border-2 border-orange-500 rounded-full md:hidden'>
+          <input type='text' name='text' id='text' placeholder='Search.....' autoComplete='off' className='flex-1 h-[5vh] px-3 focus:outline-none'/>
+          <button className='bg-gradient-to-b from-red-600 to-orange-400 text-white w-10 h-10 flex justify-center items-center rounded-full text-xl'>
+            <FaSearch />
+          </button>
+        </li>
+        </ul>
       </nav>
     </header>
   )
